a140d7db205297c24d2b721b66fadf34ee778d4a
Mozgachev I.V <mozgachev.ivan@gmail.com>
2019-02-24 14:10:21 +0300

    - add feature filter by tag messages on main page

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -5,7 +5,9 @@ import com.example.sweater.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Map;
 
@@ -26,6 +28,31 @@ public class GreetingController {
     public String main(Map<String, Object> model) {
         Iterable<Message> messages = messageRepo.findAll();
 
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping
+    public String addMessage(@RequestParam String inText, @RequestParam String inTag, Map<String, Object> model) {
+        Message message = new Message(inText, inTag);
+        messageRepo.save(message);
+
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String showMsgByTag(@RequestParam String inFilter, Map<String, Object> model) {
+        Iterable<Message> messages;
+
+        if (inFilter != null && !inFilter.isEmpty()) {
+            messages = messageRepo.findByTag(inFilter);
+        } else {
+            messages = messageRepo.findAll();
+        }
         model.put("messages", messages);
 
         return "main";
